b702c1821b3a2d5367e81b103dc0678c407c99c7
scu677 <scu677@mun.ca>
2018-01-26 01:56:02 -0330

    adding .js file

---
+++ http/templates/script.js
@@ -0,0 +1,8 @@
+function myFunc(){
+	var c1 = "Welcome to Clowder";
+document.getElementById("demo").innerHTML= c1;
+}
+function myFunc2(){
+document.getElementById("demo").innerHTML = "Welcome to Clowder";
+}
+
